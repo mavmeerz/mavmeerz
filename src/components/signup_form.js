a557f3622aadc7bd0a4c12f539013f103a4ed0df
Jearxj <jear.jujaroen@gmail.com>
2016-08-22 03:11:25 -0400

    [bug] submit button does not redirect to dashboard

--- src/components/signup_form.js
+++ src/components/signup_form.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 import {reduxForm} from 'redux-form'
-
+import '../css/signup.css'
 import {Link} from 'react-router'
 import {signup} from '../actions/authActions'
 
@@ -27,36 +27,35 @@ class SignupForm extends Component {
     return (
       <div>
         <Nav_Auth/>
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-          <p className="heading">Signup</p>
-          <div>
-            <input type="text" {...name} placeholder='name' className="biginput"/>
-            <div className="err-msg">{name.touched ? name.error : ''}</div>
-          </div>
+        <div className="signup-form">
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+            <h1>Signup</h1>
+            <div>
+              <input type="text" {...name} placeholder='name' className="biginput"/>
+              <div className="err-msg"> {name.touched ? name.error : ''}</div>
+            </div>
 
-          <div>
-            <input type="text" {...email} placeholder='email' className="biginput"/>
-            <div className="err-msg">{email.touched ? email.error : ''}</div>
-          </div>
+            <div>
+              <input type="text" {...email} placeholder='email' className="biginput"/>
+              <div className="err-msg">{email.touched ? email.error : ''}</div>
+            </div>
 
-          <div>
-            <input type="password" {...password} placeholder='password' className="biginput"/>
-            <div className="err-msg">{password.touched ? password.error : ''}</div>
-          </div>
-          <div className="btn-group">
-            <button type="submit" className="btn hvr-bounce-to-left">Submit</button>
-          </div>
-        </form><br/>
-        <Link to="/login" className="btn hvr-bounce-to-left">LOGIN</Link><br/>
+            <div>
+              <input type="password" {...password} placeholder='password' className="biginput"/>
+              <div className="err-msg">{password.touched ? password.error : ''}</div>
+            </div>
+              <button type="submit" className="button button-block btn hvr-bounce-to-left">Submit</button>
+          </form><br/>
+          <Link to="/login" className="button button-block">LOGIN</Link><br/>
+        </div>
       </div>
-
     )
   }
 }
 
 function validate(values) {
   const errors = {}
-  if(!values.name) errors.name = 'Please enter your name'
+  if(!values.name) errors.name = 'Pleae enter a valid name'
   if(!values.email) errors.email = 'Please enter a valid email'
   if(!values.password) errors.password = 'Please enter a password'
   return errors
@@ -67,3 +66,9 @@ export default reduxForm({
   fields: ['name', 'email', 'password'],
   validate
 }, null, {signup} )(SignupForm)
+
+// <div className="btn-group">
+//   <button type="submit" className="btn hvr-bounce-to-left">Submit</button>
+// </div>
+// </form>
+// <Link to="/login" className="btn hvr-bounce-to-left">LOGIN</Link><br/>*/
