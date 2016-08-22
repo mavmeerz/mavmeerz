68b38a3005006044ba9ae1aa234a85a970c40414
Jearxj <jear.jujaroen@gmail.com>
2016-08-22 03:43:33 -0400

    [style] change font and background color

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -43,7 +43,6 @@ class LoginForm extends Component {
               </div>
 
               <button type="submit" className="button button-block">Submit</button>
-
           </form><br/>
           <Link to="/signup" className="button button-block">SIGNUP</Link>
         </div>
